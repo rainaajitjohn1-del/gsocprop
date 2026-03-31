2d8483149929ffcd2b60bfd85255e7a2043640b3
rainaajitjohn1-del <rainaajitjohn1@gmail.com>
2026-03-31 09:20:12 +0000

    Initial commit

--- R/submit_proposal.R
+++ R/submit_proposal.R
@@ -7,19 +7,19 @@ SUBMISSION_DEADLINE <- as.Date("2026-03-31")
 #' @title Submit GSoC Proposal
 #' @description Checks whether the current date is before the GSoC 2026
 #'   submission deadline. The deadline is \Sexpr{format(gsocprop::SUBMISSION_DEADLINE, "%B %d, %Y")}.
+#'   \Sexpr{
+#'     days_left <- as.numeric(gsocprop::SUBMISSION_DEADLINE - Sys.Date())
+#'     if (days_left >= 0) paste("The deadline is in", days_left, "day(s).") else "The deadline has passed, try next year!"
+#'   }
 #' @return \code{TRUE} if called before the deadline, \code{FALSE} otherwise.
 #' @examples
 #' submit_proposal()
 #' @export
 submit_proposal <- function() {
   today <- Sys.Date()
-  time_left <- as.numeric(SUBMISSION_DEADLINE - today)
-  
   if (today <= SUBMISSION_DEADLINE) {
-    message("The deadline is in ", time_left, " day(s).")
     return(TRUE)
   } else {
-    message("The deadline has passed, try next year!")
     return(FALSE)
   }
 }
